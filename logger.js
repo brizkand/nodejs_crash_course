e8890b028fd722b52b5c6612d009505b2abb53ec
brizkand <brizkand27@gmail.com>
2020-12-15 18:07:40 +0800

    Initial commit

---
+++ logger.js
@@ -0,0 +1,21 @@
+const { ECANCELED } = require('constants');
+const EventEmmiter = require('events');
+const uuid = require('uuid');
+
+// //This is just to show how to generate uuid
+// console.log(uuid.v4());
+
+class Logger extends EventEmmiter{
+    log(msg){
+        //Call event
+        this.emit('message', {id: uuid.v4(), msg});
+    }
+}
+
+// module.exports = Logger;
+
+const logger = new Logger;
+
+logger.on('message', (data) => console.log('Called Listener:', data));
+
+logger.log('Hello World!');
---
+++ reference/path_demo.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+//Base file name
+console.log(path.basename(__filename));
+
+//Base directory name
+console.log(path.dirname(__filename));
+
+//File extension
+console.log(path.extname(__filename));
+
+//Create path object
+console.log(path.parse(__filename));
+
+//Concatenate path
+console.log(path.join(__dirname, 'test', 'hello.htm'));
